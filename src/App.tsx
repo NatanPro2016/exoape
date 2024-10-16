0b1103e24d1fbdfbaab4641a60bbfb9e46d98da1
NathanPro2016 <blacknatan2004@gmail.com>
2024-10-16 13:37:59 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Lenis from "lenis";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    // Listen for the scroll event and log the event data
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+  return (
+    <>
+      <div className="min-h-screen w-full bg-black text-white">hellow</div>
+      <div className="bg-red-600 min-h-screen w-full flex justify-cente items-center">
+         red
+      </div>
+      <div className="bg-blue-600 min-h-screen w-full flex justify-cente items-center text-white">
+        blue
+      </div>
+    </>
+  );
+}
+
+export default App;
